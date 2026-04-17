602b824e925a1167bd55e8d5349abdeddd6e6348
Guildbot1 <srkumar10900@gmail.com>
2026-04-17 22:20:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"Bot Running ✅")
+        self.wfile.write("Bot Running ✅".encode("utf-8"))
 
 def run_server():
     server = HTTPServer(("0.0.0.0", 10000), Handler)
